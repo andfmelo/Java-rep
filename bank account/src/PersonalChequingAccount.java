bf70cc0c7e94b638cffd7538100470bd45d57075
Andre F. Melo <71773814+andfmelo@users.noreply.github.com>
2021-04-03 16:53:46 -0400

    Update PersonalChequingAccount.java

--- bank account/src/PersonalChequingAccount.java
+++ bank account/src/PersonalChequingAccount.java
@@ -1,7 +1,7 @@
 /**
 * Program Name:  PersonalChequingAccount.java
 * Purpose: Simulate a personal chequing account.
-* Coder: Andre Florentino Melo, Student ID 0922836
+* Coder: Andre Florentino Melo
 * Date: Feb. 10, 2020
 */
 
